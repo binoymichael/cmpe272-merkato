99a56ed6cdb3f0458286b8646fd1788b440d7c32
Binoy Prakash <prakash.binoy@gmail.com>
2016-11-10 21:22:54 -0800

    Fixed port in db settings

--- config/database.php
+++ config/database.php
@@ -2,8 +2,7 @@
 $url = parse_url(getenv("DATABASE_URL"));
 
 $host = $url["host"];
-$host = $url["port"];
-$username = $url["user"];
+$port = $url["port"];
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
