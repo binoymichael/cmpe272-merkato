cb64bb454eee88bc77ae0b50c4fc2a4bec613570
Binoy Prakash <prakash.binoy@gmail.com>
2016-11-14 20:32:43 -0800

    Added models and migrations

---
+++ database/migrations/2016_11_15_034659_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('seller_id')->unsigned();
+            $table->integer('seller_product_id')->unsigned();
+            $table->integer('visited_count')->unsigned();
+            $table->timestamp('last_visited_at');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('seller_id')->references('id')->on('sellers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	DB::table('sellers')->insert([
+            'name' => 'aravinda',
+            'all_products_api' => '',
+            'show_product_api' => '',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+    	DB::table('sellers')->insert([
+            'name' => 'arun',
+            'all_products_api' => '',
+            'show_product_api' => '',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+    	DB::table('sellers')->insert([
+            'name' => 'binoy',
+            'all_products_api' => '',
+            'show_product_api' => '',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+    	DB::table('sellers')->insert([
+            'name' => 'sunil',
+            'all_products_api' => '',
+            'show_product_api' => '',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+    	DB::table('sellers')->insert([
+            'name' => 'umang',
+            'all_products_api' => '',
+            'show_product_api' => '',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
     }
 }
