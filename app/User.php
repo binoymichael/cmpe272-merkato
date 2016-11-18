a756fcf0ae216d841f6c786465184981d3424897
Binoy Prakash <prakash.binoy@gmail.com>
2016-11-18 13:58:30 -0800

    Added plumbing for recently visited products

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function products()
+    {
+        return $this->hasMany('App\Product');
+    }
 }
