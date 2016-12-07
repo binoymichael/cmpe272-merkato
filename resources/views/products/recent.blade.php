523b06734ec9c138f8c84549812f1a47f69ea04f
Binoy Prakash <prakash.binoy@gmail.com>
2016-12-06 21:06:28 -0800

    Fixed styling issues in the recent page

--- resources/views/products/recent.blade.php
+++ resources/views/products/recent.blade.php
@@ -7,22 +7,24 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Recently Visited</div>
 
-                <div class="panel-body">
+                <div class="panel-body col-md-12">
                     @foreach ($products as $product)
                          @php
                             $product_details = json_decode($product->cached_api_response, true);
                          @endphp
-                        <div class="card">
-                        <a href="/sellers/{{$product->seller_id}}/products/{{$product->seller_product_id}}">
-                          <img src="{{ urldecode($product_details['image_url']) }}" alt="Avatar" style="width:200px; height: 270px; margin-left:10px;">
-                          </a>
-                          <div class="card-container">
-                        <a href="/sellers/{{$product->seller_id}}/products/{{$product->seller_product_id}}">
-                            <h4>
-                            <b>{{ (strlen($product_details['name']) <= 18 ? $product_details['name'] : substr($product_details['name'], 0, 18) . " ...") }}</b></h4> 
-                        </a>
-                            <p>From <a href="/sellers/{{ $product->seller_id }}">{{ $product->seller->name }}</a></p> 
-                          </div>
+                         <div class="col-md-3">
+                            <div class="card">
+                            <a href="/sellers/{{$product->seller_id}}/products/{{$product->seller_product_id}}">
+                              <img src="{{ urldecode($product_details['image_url']) }}" alt="Avatar" style="width:200px; height: 270px;">
+                              </a>
+                              <div class="card-container">
+                            <a href="/sellers/{{$product->seller_id}}/products/{{$product->seller_product_id}}">
+                                <h4>
+                                <b>{{ (strlen($product_details['name']) <= 18 ? $product_details['name'] : substr($product_details['name'], 0, 18) . " ...") }}</b></h4> 
+                            </a>
+                                <p>From <a href="/sellers/{{ $product->seller_id }}">{{ $product->seller->name }}</a></p> 
+                              </div>
+                            </div>
                         </div>
                     @endforeach
                 </div>
